e2749a5c9be0e78baa4a61afd1fec17e8f9b5089
devaeterne <info@safaksariyildiz.com>
2024-12-18 02:09:43 +0100

    test

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -1,86 +1,16 @@
 "use strict";
 
-var _require = require('http'),
-    createServer = _require.createServer;
+var jsonServer = require("json-server"); // json-server modülünü çağırıyoruz
 
-var _require2 = require('url'),
-    parse = _require2.parse;
 
-var next = require('next');
+var server = jsonServer.create();
+var router = jsonServer.router("db.json"); // Kullanmak istediğiniz JSON dosyası
 
-var dev = process.env.NODE_ENV !== 'production';
-var hostname = 'localhost';
-var port = process.env.port || 3000; // when using middleware `hostname` and `port` must be provided below
+var middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(router);
+var port = process.env.PORT || 3001; // Port numarasını belirliyoruz
 
-var app = next({
-  dev: dev,
-  hostname: hostname,
-  port: port
-});
-var handle = app.getRequestHandler();
-app.prepare().then(function () {
-  createServer(function _callee(req, res) {
-    var parsedUrl, pathname, query;
-    return regeneratorRuntime.async(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.prev = 0;
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            parsedUrl = parse(req.url, true);
-            pathname = parsedUrl.pathname, query = parsedUrl.query;
-
-            if (!(pathname === '/a')) {
-              _context.next = 8;
-              break;
-            }
-
-            _context.next = 6;
-            return regeneratorRuntime.awrap(app.render(req, res, '/a', query));
-
-          case 6:
-            _context.next = 15;
-            break;
-
-          case 8:
-            if (!(pathname === '/b')) {
-              _context.next = 13;
-              break;
-            }
-
-            _context.next = 11;
-            return regeneratorRuntime.awrap(app.render(req, res, '/b', query));
-
-          case 11:
-            _context.next = 15;
-            break;
-
-          case 13:
-            _context.next = 15;
-            return regeneratorRuntime.awrap(handle(req, res, parsedUrl));
-
-          case 15:
-            _context.next = 22;
-            break;
-
-          case 17:
-            _context.prev = 17;
-            _context.t0 = _context["catch"](0);
-            console.error('Error occurred handling', req.url, _context.t0);
-            res.statusCode = 500;
-            res.end('internal server error');
-
-          case 22:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, null, null, [[0, 17]]);
-  }).once('error', function (err) {
-    console.error(err);
-    process.exit(1);
-  }).listen(port, function () {
-    console.log("> Ready on http://".concat(hostname, ":").concat(port));
-  });
+server.listen(port, function () {
+  console.log("JSON Server is running on port ".concat(port));
 });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); // json-server modülünü çağırıyoruz
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json"); // Kullanmak istediğiniz JSON dosyası
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; // Port numarasını belirliyoruz
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
